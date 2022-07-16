d48ec762c5ba5a9124fd2df2cae8d8d7e788617d
rahul31agrawal <rahul31agrawal@gmail.com>
2022-07-16 11:03:26 +0530

    Auth feature added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import "./App.css";
 
-import Home from "./pages/homePage/Home"
+import Home from "./pages/homePage/Home";
 import { Routes, Route } from "react-router-dom";
-import {ProductListing} from "./pages/productListing/ProductListing";
-import {WishlistPage} from "./pages/wishListPage/WishListPage";
+import { ProductListing } from "./pages/productListing/ProductListing";
+import { WishlistPage } from "./pages/wishListPage/WishListPage";
 import CartPage from "./pages/cartPage/CartPage";
-
-
-
+import { LoginPage } from "./pages/loginPage/login";
+import { RequiresAuth } from "./context/RequiresAuth";
+import { SignupPage } from "./pages/signupPage/signup";
 
 function App() {
   return (
     <div className="App">
-    
-    <Routes>
+      <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/ProductListing" element={<ProductListing/>} />
-        <Route path="/WishlistPage" element={<WishlistPage/>} />
-        <Route path="/CartPage" element={<CartPage/>} />
-    </Routes>
+        <Route path="/ProductListing" element={<ProductListing />} />
+        <Route path="/WishlistPage" element={<WishlistPage />} />
 
+        <Route
+          path="/CartPage"
+          element={
+            <RequiresAuth>
+              <CartPage />
+            </RequiresAuth>
+          }
+        />
+
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -7,20 +7,22 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { ProductProvider } from "./context/Product-context";
 import { CartProvider } from "./context/cart-context";
 import { WishlistProvider } from "./context/wishlist-context";
-
+import { AuthContextProvider } from "./context/authcontext";
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-    <ProductProvider>
-      <CartProvider>
-       <WishlistProvider>
-           <App />
-        </WishlistProvider>
-      </CartProvider>
-    </ProductProvider>
+      <AuthContextProvider>
+        <ProductProvider>
+          <CartProvider>
+            <WishlistProvider>
+              <App />
+            </WishlistProvider>
+          </CartProvider>
+        </ProductProvider>
+      </AuthContextProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
